fa93c0682973a9f3ec39943a6b6cf78401306199
Allen <almcbroombey@hotmail.com>
2022-04-10 04:10:13 -0400

    updated js file

--- Docs/Assets/js/script.js
+++ Docs/Assets/js/script.js
@@ -24,21 +24,21 @@ THEN the saved events persist*/
 //link to elements on the page one single selector
 //use this Statements
       
-const now = moment();
-console.log(now.format("dddd, MMMM Do YYYY, h:mm:ss a"));
-document.getElementById("currentDay").textContent = now.format("dddd, MMMM Do YYYY, h:mm:ss a");  
+// const now = moment();
+// console.log(now.format("dddd, MMMM Do YYYY, h:mm:ss a"));
+// document.getElementById("currentDay").textContent = now.format("dddd, MMMM Do YYYY, h:mm:ss a"); 
 
+//this.siblings() 
 
-
-// value of date
+Assign value of date
 let todayDate = moment().format('dddd, MMM Do YYYY, HH');
 
-//Format date displayed to include hour (0000 to 2300)
+Format date displayed to include hour (0000 to 2300)
 $("#currentDay").html(todayDate + "00");
 
-//Initialize when ready
+Initialize when ready
 $(document).ready(function () {
-        //Retrieve local storage data if present
+        Retrieve local storage data if present
         $("#8-hour .task").val(localStorage.getItem("8-hour"));
         $("#9-hour .task").val(localStorage.getItem("9-hour"));
         $("#10-hour .task").val(localStorage.getItem("10-hour"));
@@ -53,41 +53,41 @@ $(document).ready(function () {
         $("#7-hour .task").val(localStorage.getItem("7-hour"));
        
     
-    /////////////Save-button///////////////////////////////  
+    Save-button click event listener 
     $(".save-button").on("click", function () {
-        // Get nearby values of the description in JQuery
+        Get nearby values of the description in JQuery
         let task = $(this).siblings(".task").val();
         let hour = $(this).parent().attr("id");
 
-        // Save task to local storage
+        Save task to local storage
         localStorage.setItem(hour, task);
     })
    
-    //Begin hourKeeper function
+    Begin hourKeeper function
     function hourKeeper() {
 
-        //Get the current hour
+        Get the current hour
         let currentHour = moment().hour();
 
-        //Loop over the hour blocks
+        Loop over the hour blocks
         $(".hour-block").each(function () {
             let hourBlock = parseInt($(this).attr("id").split("-hour")[0]);
 
-            //Compare hour block to current hour
+            Compare hour block to current hour
             if (hourBlock < currentHour) {
-                //Set background for past hour block
+                Set background for past hour block
                 $(this).removeClass("future");
                 $(this).removeClass("present");
                 $(this).addClass("past");
             }
             else if (hourBlock === currentHour) {
-                //Set background for tasks current hour block
+                Set background for tasks current hour block
                 $(this).removeClass("past");
                 $(this).removeClass("future");
                 $(this).addClass("present");
             }
             else {
-                //Set background for future hour blocks
+                Set background for future hour blocks
                 $(this).removeClass("present");
                 $(this).removeClass("past");
                 $(this).addClass("future");
@@ -95,7 +95,105 @@ $(document).ready(function () {
             }
         });
     }
-    //Initialize hourKeeper
+    Initialize hourKeeper
     hourKeeper();
 })
-*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+Assign value of date
+let todayDate = moment().format('dddd, MMM Do YYYY, HH');
+
+Format date displayed to include hour (0900AM to 0700PM)
+$("#currentDay").html(todayDate + "00");
+
+Initialize when ready//// this works also $(function() {
+$(document).ready(function () {
+        //Retrieve local storage data if present
+        $("#9-hour .task").val(localStorage.getItem("9-AM"));
+        $("#10-hour .task").val(localStorage.getItem("10-AM"));
+        $("#11-hour .task").val(localStorage.getItem("11-AM"));
+        $("#12-hour .task").val(localStorage.getItem("12-PM"));
+        $("#1-hour .task").val(localStorage.getItem("1-PM"));
+        $("#2-hour .task").val(localStorage.getItem("2-PM"));
+        $("#3-hour .task").val(localStorage.getItem("3-PM"));
+        $("#4-hour .task").val(localStorage.getItem("4-PM"));
+        $("#5-hour .task").val(localStorage.getItem("5-PM"));
+        $("#6-hour .task").val(localStorage.getItem("6-PM"));
+        $("#7-hour .task").val(localStorage.getItem("7-PM"));
+    
+    Save-button click event listener 
+    $(".save-button").on("click", function () {
+        Get nearby values of the description in JQuery
+        let task//+project+// = $(this).siblings(".task").val();
+        let hour = $(this).parent().attr("id");
+
+        Save task to local storage
+        localStorage.setItem(hour, task);
+    })
+   
+    Begin hourKeeper function
+    function hourKeeper() {
+
+        Get the current hour
+    ,    let currentHour = moment().hour();
+
+        Loop over the hour blocks
+        $(".hour-block").each(function () {
+            let hourBlock = parseInt($(this).attr("id").split("-hour")[0]);
+
+            Compare hour block to current hour
+            if (hourBlock < currentHour) {
+                Set background for past hour block
+                $(this).removeClass("future");
+                $(this).removeClass("present");
+                $(this).addClass("past");
+            }
+            else if (hourBlock === currentHour) {
+                Set background for tasks current hour block
+                $(this).removeClass("past");
+                $(this).removeClass("future");
+                $(this).addClass("present");
+            }
+            else {
+                Set background for future hour blocks
+                $(this).removeClass("present");
+                $(this).removeClass("past");
+                $(this).addClass("future");
+
+            }
+        });
+    }
+Initialize hourKeeper
+    hourKeeper();
+})
