b1ab18613a81beb8a6af4b6d107659e20bab2230
Allen <almcbroombey@hotmail.com>
2022-04-12 19:21:58 -0400

    change clear

--- Docs/Assets/js/script.js
+++ Docs/Assets/js/script.js
@@ -22,15 +22,15 @@ WHEN I refresh the page
 THEN the saved events persist*/
 
 
-Assign value of date
+//Assign value of date
 let todayDate = moment().format('dddd, MMM Do YYYY, HH');
 
-Format date displayed to include hour (0900AM to 0700PM)
+//Format date displayed to include hour (0900AM to 0700PM)
 $("#currentDay").html(todayDate + "00");
 
 
-Initialize when ready//// this works also $(function() {
-$(document).ready(function () {
+//Initialize when ready//// this works also $(function() {
+//$(document).ready(function () {
         //Retrieve local storage data if present
         $("#9-hour .task").val(localStorage.getItem("9-AM"));
         $("#10-hour .task").val(localStorage.getItem("10-AM"));
@@ -44,45 +44,43 @@ $(document).ready(function () {
         $("#6-hour .task").val(localStorage.getItem("6-PM"));
         $("#7-hour .task").val(localStorage.getItem("7-PM"));
     
-    Save-button click.event listener 
+    //Save-button click.event listener 
     $(".save-button").on("click", function () {
-        Get nearby values of the description in JQuery
-        let task//+project+// = $(this).siblings(".task").val();
+        //Get nearby values of the description in JQuery
         let hour = $(this).parent().attr("id");
 
-        Save task to local storage
+        //Save task to local storage
         localStorage.setItem(hour, task);
     })
    
-    Begin hourKeeper function
+    //Begin hourKeeper function
     function hourKeeper() {
 
-        Get the current hour
-    ,    let currentHour = moment().hour();
+        //Get the current hour
+        let currentHour = moment().hour();
 
-        Loop over the hour blocks
+        //SetLoop over the hour blocks
         $(".hour-block").each(function () {
             let hourBlock = parseInt($(this).attr("id").split("-hour")[0]);
 
-            Compare hour block to current hour
+            //Compare hour block to current hour
             if (hourBlock < currentHour) {
-                Set background for past hour block
+                //Set background for past hour block
                 $(this).removeClass("future");
                 $(this).removeClass("present");
                 $(this).addClass("past");
             }
             else if (hourBlock === currentHour) {
-                Set background for tasks current hour block
+                //Set background for tasks current hour block
                 $(this).removeClass("past");
                 $(this).removeClass("future");
                 $(this).addClass("present");
             }
             else {
-                Set background for future hour blocks
+                //Set background for future hour blocks
                 $(this).removeClass("present");
                 $(this).removeClass("past");
                 $(this).addClass("future");
-
             }
         });
     }
