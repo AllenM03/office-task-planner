7fbf0adfd45682ab89fdff06e97acc63ca3dc6a6
Allen <almcbroombey@hotmail.com>
2022-04-17 23:30:00 -0400

    corrected save to local storage function in js file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,8 @@
-// Used const for these as they are not changed
+//Global declaration constants
 const timeBlockEl = $('.timeblock');
 const buttonEl = $('.btn');
-// counts the timeblocks in the HTML file and would be used in the for loops in several functions
+
+//counts the timeblocks in the HTML file and would be used in the for loops
 const blockCount = timeBlockEl.children().length;
 
 //Adds the date to the jumbotron
@@ -11,52 +12,51 @@ function setDate() {
     
 }
 
-// sets background color of the timeblocks depending on the current time
+//Sets background color of the timeblocks depending on the current time
 function checkTimeBlock() {    
     var currentHour = moment().format('MM-DD-YYYY h:mm A');
     var timeBlock = '';
     var timeBlockID = '';
 
     for (var i = 0; i < blockCount; i++) {
-        //get id of the textarea (child of the div in the class timeblock) and transform it as a datetime string value
+        //retrieves id of the textarea and makes it a datetime string value
         timeBlockID = $(".timeblock div textarea").eq(i).attr("id");
         timeBlock = moment(moment().format('MM-DD-YYYY')+' '+moment(timeBlockID,'hA').format('h:mm A'),'MM-DD-YYYY h:mm A');
         timeBlock = checkTimeDiff(currentHour, timeBlock);
 
         if (timeBlock < 0) {
-            // for timeblocks greater than current time changes to green
+            //timeblocks greater than current time changes to green
             $(`#${timeBlockID}`).addClass('bg-success');
         } else if (timeBlock > 1) {
-            // for timeblocks less than current time changes to gray
+            //timeblocks less than current time changes to gray
             $(`#${timeBlockID}`).addClass('bg-secondary');
         } else {
-            // for timeblocks equal to current time changes to red
+            //timeblocks equal to current time changes to red
             $(`#${timeBlockID}`).addClass('bg-danger');
         }
     }
 }
 
-// function for comparing the current time with the timeblock and returns their difference in hours
+//function for comparing the present time with the timeblock and shows their difference in hours
 function checkTimeDiff(currentHour, timeBlock) {
     var timeDiff = 0;
-    // get difference of timeblock time to the current time in hours
     var timeDiff = moment(currentHour,'MM-DD-YYYY h:mm A').diff(timeBlock, 'hours',true);
     return timeDiff;
 }
 
-// loads the events stored in localStorage
+//loads the events stored in localStorage
 function printSavedSched() {
     var textArea = '';
     var storedVal = '';
     var currDate =  moment().format('MM-DD-YYYY');
 
-    // check all timeblocks (child of div that is a child of class timeblock) if there's a corresponding key in localStorage
+    //check all timeblocks if there's a similar key in localStorage
     for (var i = 0; i < blockCount; i++) {
         textArea = $(".timeblock div textarea").eq(i).attr("id");
-        // use value of currentdate and timeBlock as search selector
+        //use value of currentdate and timeBlock as search selector
         storedVal = localStorage.getItem(`${currDate} ${textArea}`);
 
-        // if it is found, load it to textArea
+    //if it is found, load it to textArea
     if (storedVal) {
         $(`#${textArea}`).val(storedVal);
         }
@@ -72,7 +72,7 @@ timeBlockEl.on('click', '.btn', function (event) {
     localStorage.setItem(`${currDate} ${timeBlock}`, textAreaValue);
 })
 
-// do all of these when the page has finished loading
+//function runs these commands when the page has finished loading
 function init() {
     setDate();
     checkTimeBlock();
